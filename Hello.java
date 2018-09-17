26617f5108a7be172d9ad8b3234bf7bbe7f07247
isulley77 <isulley@stuy.edu>
2018-09-17 00:38:37 -0400

    Hello World Program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+ public class Hello{
+   public static void main(String args[]) {
+     String x = "Ian Sulley";
+     String y = "Put on some golf shoes! Otherwise we'll never make it out of this place alive";
+
+     System.out.println("Hello my name is " + x + ", " + y);
+
+
+   }
+ }
